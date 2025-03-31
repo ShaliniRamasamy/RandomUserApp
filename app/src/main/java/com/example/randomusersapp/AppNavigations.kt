f33bcaa7fce249bc12f4a19fda63ed0666cd303d
Shalini <shalini.ramu91092@gmail.com>
2025-03-31 10:34:54 +0530

    UI changes - warning fix

--- app/src/main/java/com/example/randomusersapp/AppNavigations.kt
+++ app/src/main/java/com/example/randomusersapp/AppNavigations.kt
@@ -3,13 +3,11 @@ package com.example.randomusersapp
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.randomusersapp.domain.model.Result
